01a58ecf07b858b56deb4ed3c15787c446aedc26
zhm <306490609@qq.com>
2017-11-28 01:18:42 +0800

    -Optimize code

--- Main.php
+++ Main.php
@@ -21,50 +21,145 @@ function entry()
 //            echo "\n";
 //        }
 //    }
-
-    $result = processInput($frames);
+    $start_time = microtime(true);
+    $result1 = processInput1($frames);
+    $end_time = microtime(true);
+    echo "ProcessInput1 Time uses:" . ($end_time - $start_time);
     for ($row = 0; $row < 10; $row++) {
-        echo $result[$row] . " ";
+        echo 'result1:' . $result1[$row] . " ";
+    }
+    echo "\n";
+
+    $start_time = microtime(true);
+    $result2 = processInput2($frames);
+    $end_time = microtime(true);
+    echo "ProcessInput2 Time uses:" . ($end_time - $start_time);
+    for ($row = 0; $row < 10; $row++) {
+        echo 'result2:' . $result2[$row] . " ";
     }
 }
 
-function processInput($input)
+
+function processInput1($input)
 {
     $result = array();
+
     $total_score = 0;
 
     for ($frame = 0; $frame < 10; $frame++) {
-//        echo "Frame number" . $frame;
-//        echo "\n";
-        for ($throw = 0; $throw < 3; $throw++) {
-//            echo $input[$frame][$throw];
-//            echo "\n";
-            $current_score = $input[$frame][$throw];
 
-            $total_score += $current_score;
+        foreach ($input[$frame] as $current_score) {
+
             if ($current_score > 10 || $current_score < 0) {
-//                echo "Invalid score!";
-//                echo "\n";
-            } else if ($current_score == 10) {
-                for ($t = $frame + 1; $t < $frame + 3 && $t < 10; $t++) {
-                    for (; $throw < 3; $throw++) {
-                        $total_score += $input[$t][$throw];
-                        if ($input[$t][$throw] == 10) {
-                            for (; $throw < 3; $throw++) {
-                                $total_score += $input[$t + 1][$throw];
-                            }
+                echo "Invalid score!";
+                echo "\n";
+            } else {
+                $total_score += $current_score;
+                if ($current_score == 10) {
 
+                    foreach ($input[$frame + 1] as $next_score) {
+
+                        $total_score += $next_score;
+                        if ($next_score == 10) {
+
+                            foreach ($input[$frame + 2] as $score) {
+                                $total_score += $score;
+                            }
                         }
                     }
                 }
-            } else {
-
             }
-
         }
         array_push($result, $total_score);
     }
     return $result;
+}
+
+function addAdditionalScore($current_score, $arr, $frame)
+{
+    $additional_score = 0;
+    $t=0;
+    if ($current_score != 10) return $additional_score;
+    if ($current_score == 10) {
+        $t++;
+        foreach ($arr[$frame+$t] as $score) {
+            $additional_score += $score;
+            $additional_score+=addAdditionalScore($score, $arr, $frame+2);
+        }
+        return $additional_score;
+    }
+}
+
+function processInput2($input)
+{
+    $result = array();
+
+    $total_score = 0;
+
+//    for ($frame = 0; $frame < 10; $frame++) {
+////        echo "Frame number" . $frame;
+////        echo "\n";
+//        for ($throw = 0; $throw < 3; $throw++) {
+////            echo $input[$frame][$throw];
+////            echo "\n";
+//            $current_score = $input[$frame][$throw];
+//
+//            if ($current_score > 10 || $current_score < 0) {
+//                echo "Invalid score!";
+//                echo "\n";
+//            } else {
+//                $total_score += $current_score;
+//
+//                if ($current_score == 10) {
+//                    for (; $throw < 3; $throw++) {
+//                        $next_score = $input[$frame + 1][$throw];
+//                        $total_score += $next_score;
+//                        if ($next_score == 10) {
+//                            for (; $throw < 3; $throw++) {
+//                                $total_score += $input[$frame + 2][$throw];
+//                            }
+//
+//                        }
+//                    }
+//                }
+//            }
+//
+//        }
+//        array_push($result, $total_score);
+//    }
+    for ($frame = 0; $frame < 10; $frame++) {
+
+        foreach ($input[$frame] as $current_score) {
+
+            if ($current_score > 10 || $current_score < 0) {
+                echo "Invalid score!";
+                echo "\n";
+            } else {
+                $total_score += $current_score;
+                $total_score += addAdditionalScore($current_score,$input,$frame);
+            }
+        }
+        array_push($result, $total_score);
+    }
+    return $result;
+}
+
+function arr_foreach($arr)
+{
+    if (!is_array($arr)) {
+        return false;
+    }
+
+    foreach ($arr as $key => $val) {
+        if (is_array($val)) {
+            arr_foreach($val);
+        } else {
+            echo 'key:' . $key . 'val:' . $val . ' ';
+            echo "\n";
+            echo '$arr:' . $arr[$key + 1];
+        }
+        echo "\n";
+    }
 }
 
 entry();
--- Model.php
+++ Model.php
@@ -25,19 +25,21 @@ class Model
     {
         $frames = array
         (
-//            array(5, 2),
-//            array(8, 1),
-//            array(6, 4),
-            array(10),
-            array(10),
-            array(10),
+            array(5, 2),
+            array(8, 1),
+            array(6, 4),
+//            array(10),
+//            array(10),
+//            array(10),
             array(10),
             array(0, 5),
             array(2, 6),
             array(8, 1),
-            array(5, 3),
-            array(6, 1),
-            array(10, 2, 6)
+//            array(5, 3),
+//            array(6, 1),
+            array(10),
+            array(10),
+            array(10, 10, 6)
         );
         return $frames;
     }
